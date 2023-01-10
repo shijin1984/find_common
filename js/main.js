ec16e54818f31b4d23bffff3ac24db33ba034fb0
Jin Shi <jinshi@google.com>
2023-01-10 10:02:45 +0800

    [dev] set up the layout.

--- js/main.js
+++ js/main.js
@@ -9,14 +9,24 @@ $(function() {
       var parentId = position + "_td";
       var width = $("#" + parentId).width();
       var height = $("#" + parentId).height();
-      var size = Math.min(0.5*width, 0.5*height);
+      var size = Math.min(width/2, height/2);
 
       var nodes = [];
       for (var i = 0; i < 3; ++i) {
         var node = $("<div></div>", { id: position + "_fig_" + i});
         node.text(i + 1);
+        node.css({
+          width: size + "px",
+          height: size + "px",
+          "font-size": size + "px",
+        });
+
         nodes.push(node);
       }
+
+      nodes[0].css("margin-left", (width / 4) + "px");
+      nodes[1].css({float: "left", width: "50%"});
+      nodes[2].css({float: "left", width: "50%"});
 
       for (var i = 0; i < 3; ++i) {
         $("#" + parentId).append(nodes[i]);
